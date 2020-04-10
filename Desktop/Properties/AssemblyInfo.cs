bb0888f89e7545f221ab1d038ae3ada245999942
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-04-10 02:41:02 +0200

    Renombrado de Desktop a Mauzo Desktop.

--- Desktop/Properties/AssemblyInfo.cs
+++ Desktop/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Desktop")]
+[assembly: AssemblyTitle("Mauzo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Desktop")]
+[assembly: AssemblyProduct("Mauzo Desktop")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
