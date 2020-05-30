97801b9ec6207c13a245aff12e1e7a70bbd5a8cb
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-05-30 18:12:07 +0200

    Desktop - Templates nuevas añadidas.

---
+++ Desktop/Templates/Discount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Templates
+{
+    class Discount
+    {
+        private int id;
+        private string code;
+        private string desc;
+        private float priceDisc;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Code
+        {
+            get => code;
+            set => code = value;
+        }
+
+        public string Desc
+        {
+            get => desc;
+            set => desc = value;
+        }
+
+        public float PriceDisc
+        {
+            get => priceDisc;
+            set => priceDisc = value;
+        }
+    }
+}
---
+++ Desktop/Templates/Informs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Templates
+{
+    class Informs
+    {
+        private int id;
+        private int nSales;
+        private int nRefunds;
+        private int nDiscounts;
+
+        private DateTime dStart;
+        private DateTime dEnd;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+        public int NSales 
+        {
+            get => nSales;
+            set => nSales = value;
+        }
+        public int NRefunds
+        {
+            get => nRefunds;
+            set => nRefunds = value;
+        }
+        public int NDiscounts 
+        {
+            get => nDiscounts;
+            set => nDiscounts = value;
+        }
+
+        public DateTime DStart
+        {
+            get => dStart;
+            set => dStart = value;
+        }
+
+        public DateTime DEnd
+        {
+            get => dEnd;
+            set => dEnd = value;
+        }
+    }
+}
